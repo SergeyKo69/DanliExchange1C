2c7d5879c4fc3ef7519acb7f02077ac04bf09d86
Kogut <koguts@yandex.ru>
2021-07-22 22:31:07 +0300

    Initial

---
+++ src/main/java/com/kogut/danliexchange1c/DanliExchange1CApplication.java
@@ -0,0 +1,13 @@
+package com.kogut.danliexchange1c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanliExchange1CApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanliExchange1CApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kogut/danliexchange1c/DanliExchange1CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kogut.danliexchange1c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanliExchange1CApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
