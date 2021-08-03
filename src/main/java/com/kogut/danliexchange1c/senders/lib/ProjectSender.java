40176ba057743e55f210894a01d92958533ee65e
Kogut <koguts@yandex.ru>
2021-08-03 18:40:19 +0300

    refactoring project sender

--- src/main/java/com/kogut/danliexchange1c/senders/lib/ProjectSender.java
+++ src/main/java/com/kogut/danliexchange1c/senders/lib/ProjectSender.java
@@ -1,12 +1,18 @@
 package com.kogut.danliexchange1c.senders.lib;
 
 import com.kogut.danliexchange1c.dto.lib.project.ProjectDTO;
+import com.kogut.danliexchange1c.enumerations.general.ClientDBEnum;
 import com.kogut.danliexchange1c.senders.interfaces.ISender;
 import com.kogut.danliexchange1c.util.interfaces.ISettingsUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 /**
  * @author S.Kogut on 03.08.2021
@@ -26,6 +32,18 @@ public class ProjectSender implements ISender<ProjectDTO> {
 
     @Override
     public HttpStatus send(ProjectDTO projectDTO) {
-        return null;
+        HashMap<String, String> settings = settingsUtil.getSettings(ClientDBEnum.valueOf(projectDTO.getClientDB()));
+        StringBuilder url = new StringBuilder();
+        url.append(settings.get("address")).append("/").append(settings.get("pathProject"));
+        HttpEntity<ProjectDTO> request = new HttpEntity<>(projectDTO, settingsUtil.createHeaders(settings.get("userName"), settings.get("password")));
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(
+                    url.toString(),
+                    request,
+                    String.class);
+            return response.getStatusCode();
+        } catch (IllegalArgumentException | RestClientException e) {
+            return HttpStatus.BAD_REQUEST;
+        }
     }
 }
