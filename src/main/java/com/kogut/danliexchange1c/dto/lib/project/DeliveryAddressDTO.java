4c8f11235bf294aebdabf3f5d163a146a2b43604
Kogut <koguts@yandex.ru>
2021-08-03 19:07:48 +0300

    add delivery address dto

---
+++ src/main/java/com/kogut/danliexchange1c/dto/lib/project/DeliveryAddressDTO.java
@@ -0,0 +1,14 @@
+package com.kogut.danliexchange1c.dto.lib.project;
+
+import com.kogut.danliexchange1c.dto.lib.common.AbstractCommonLibDTO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author S.Kogut on 03.08.2021
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class DeliveryAddressDTO extends AbstractCommonLibDTO {
+}
