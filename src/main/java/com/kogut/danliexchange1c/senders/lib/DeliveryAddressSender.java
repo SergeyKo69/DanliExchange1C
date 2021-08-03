21aad8b0dd85e627b56abf1e4417481f7bdc314c
Kogut <koguts@yandex.ru>
2021-08-03 19:32:34 +0300

    add delivery address sender

---
+++ src/main/java/com/kogut/danliexchange1c/senders/lib/DeliveryAddressSender.java
@@ -0,0 +1,46 @@
+package com.kogut.danliexchange1c.senders.lib;
+
+import com.kogut.danliexchange1c.dto.lib.project.DeliveryAddressDTO;
+import com.kogut.danliexchange1c.enumerations.general.ClientDBEnum;
+import com.kogut.danliexchange1c.senders.interfaces.ISender;
+import com.kogut.danliexchange1c.util.interfaces.ISettingsUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+
+/**
+ * @author S.Kogut on 03.08.2021
+ */
+public class DeliveryAddressSender implements ISender<DeliveryAddressDTO> {
+
+    private final RestTemplate restTemplate;
+    private final ISettingsUtil settingsUtil;
+
+    @Autowired
+    public DeliveryAddressSender(RestTemplate restTemplate, ISettingsUtil settingsUtil) {
+        this.restTemplate = restTemplate;
+        this.settingsUtil = settingsUtil;
+    }
+
+    @Override
+    public HttpStatus send(DeliveryAddressDTO deliveryAddressDTO) {
+        HashMap<String, String> settings = settingsUtil.getSettings(ClientDBEnum.valueOf(deliveryAddressDTO.getClientDB()));
+        StringBuilder url = new StringBuilder();
+        url.append(settings.get("address")).append("/").append(settings.get("pathDeliveryAddress"));
+        HttpEntity<DeliveryAddressDTO> request = new HttpEntity<>(deliveryAddressDTO, settingsUtil.createHeaders(settings.get("userName"), settings.get("password")));
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(
+                    url.toString(),
+                    request,
+                    String.class);
+            return response.getStatusCode();
+        } catch (IllegalArgumentException | RestClientException e) {
+            return HttpStatus.BAD_REQUEST;
+        }
+    }
+}
