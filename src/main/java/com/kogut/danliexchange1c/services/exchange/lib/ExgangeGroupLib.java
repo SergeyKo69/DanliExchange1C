433bca03194d80e22f636146b01215740583b040
Kogut <koguts@yandex.ru>
2021-08-09 23:01:24 +0300

    add group lib exchange

--- src/main/java/com/kogut/danliexchange1c/services/exchange/lib/ExgangeGroupLib.java
+++ src/main/java/com/kogut/danliexchange1c/services/exchange/lib/ExgangeGroupLib.java
@@ -1,9 +1,11 @@
 package com.kogut.danliexchange1c.services.exchange.lib;
 
 import com.kogut.danliexchange1c.dto.lib.common.GroupLibDTO;
-import com.kogut.danliexchange1c.dto.lib.project.ProjectDTO;
 import com.kogut.danliexchange1c.senders.interfaces.ISender;
+import com.kogut.danliexchange1c.services.db.lib.interfaces.IGroupLibService;
 import com.kogut.danliexchange1c.services.exchange.interfaces.IExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,8 +15,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class ExgangeGroupLib implements IExchange<GroupLibDTO> {
 
+    private final ISender<GroupLibDTO> sender;
+    private final IGroupLibService groupService;
+
+    @Autowired
+    public ExgangeGroupLib(ISender<GroupLibDTO> sender, IGroupLibService groupService) {
+        this.sender = sender;
+        this.groupService = groupService;
+    }
+
     @Override
     public void exchange(GroupLibDTO groupLibDTO) {
-
+        groupService.deleteByExternalId(groupLibDTO.getExternalId());
+        HttpStatus status = sender.send(groupLibDTO);
+        if (status != HttpStatus.CREATED) {
+            // Save database.
+            groupService.saveDTO(groupLibDTO);
+        }
     }
 }
