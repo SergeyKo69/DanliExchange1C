9f14239caafeb946c9f22f4f52b0625d667a7586
Kogut <koguts@yandex.ru>
2021-08-09 23:10:10 +0300

    add bank bill dto

---
+++ src/main/java/com/kogut/danliexchange1c/dto/lib/client/BankBillDTO.java
@@ -0,0 +1,33 @@
+package com.kogut.danliexchange1c.dto.lib.client;
+
+import com.kogut.danliexchange1c.dto.lib.common.AbstractCommonLibDTO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * @author S.Kogut on 09.08.2021
+ */
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class BankBillDTO extends AbstractCommonLibDTO {
+
+    @NotEmpty
+    private String typeOwner = "";
+
+    @NotEmpty
+    private String bik = "";
+
+    @NotEmpty
+    private String viewBill = "";
+
+    @NotEmpty
+    private String numberBill = "";
+
+    private String currencyId = "";
+
+}
