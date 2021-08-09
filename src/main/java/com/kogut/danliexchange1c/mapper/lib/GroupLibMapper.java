348170c5e41a36084f4b746611e5141a9ac39713
Kogut <koguts@yandex.ru>
2021-08-09 22:45:31 +0300

    add group lib mapper

---
+++ src/main/java/com/kogut/danliexchange1c/mapper/lib/GroupLibMapper.java
@@ -0,0 +1,50 @@
+package com.kogut.danliexchange1c.mapper.lib;
+
+import com.kogut.danliexchange1c.dto.lib.common.GroupLibDTO;
+import com.kogut.danliexchange1c.dto.lib.project.ProjectDTO;
+import com.kogut.danliexchange1c.enumerations.general.ClientDBEnum;
+import com.kogut.danliexchange1c.enumerations.lib.TypeOwnerGroupLibEnum;
+import com.kogut.danliexchange1c.mapper.abstracted.AbstractMapper;
+import com.kogut.danliexchange1c.model.lib.common.GroupLibEntity;
+import com.kogut.danliexchange1c.model.lib.project.ProjectEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author S.Kogut on 09.08.2021
+ */
+
+@Component
+public class GroupLibMapper extends AbstractMapper<GroupLibEntity, GroupLibDTO>  {
+
+    private final ModelMapper mapper;
+
+    @Autowired
+    public GroupLibMapper(ModelMapper mapper) {
+        super(GroupLibEntity.class, GroupLibDTO.class);
+        this.mapper = mapper;
+    }
+
+    @PostConstruct
+    public void setupMapper() {
+        mapper.createTypeMap(GroupLibDTO.class, GroupLibEntity.class)
+                .addMappings(m -> m.skip(GroupLibEntity:: setId))
+                .addMappings(m -> m.skip(GroupLibEntity::setClientDB));
+    }
+
+    @Override
+    public void mapSpecificFields(GroupLibEntity source, GroupLibDTO destination) {
+        super.mapSpecificFields(source, destination);
+        destination.setTypeOwnerGroup(source.getTypeOwnerGroup().toString());
+    }
+
+    @Override
+    public void mapSpecificFields(GroupLibDTO source, GroupLibEntity destination) {
+        super.mapSpecificFields(source, destination);
+        destination.setClientDB(ClientDBEnum.valueOf(source.getClientDB()));
+        destination.setTypeOwnerGroup(TypeOwnerGroupLibEnum.valueOf(source.getTypeOwnerGroup()));
+    }
+}
