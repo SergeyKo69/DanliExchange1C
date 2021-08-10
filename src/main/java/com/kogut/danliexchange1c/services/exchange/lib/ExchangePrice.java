acda66caddfe005d5580c67deea9a374319d4f17
Kogut <koguts@yandex.ru>
2021-08-10 16:55:33 +0300

    add price exchange

---
+++ src/main/java/com/kogut/danliexchange1c/services/exchange/lib/ExchangePrice.java
@@ -0,0 +1,36 @@
+package com.kogut.danliexchange1c.services.exchange.lib;
+
+import com.kogut.danliexchange1c.dto.lib.price.PriceDTO;
+import com.kogut.danliexchange1c.senders.interfaces.ISender;
+import com.kogut.danliexchange1c.services.db.lib.interfaces.IPriceService;
+import com.kogut.danliexchange1c.services.exchange.interfaces.IExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author S.Kogut on 10.08.2021
+ */
+
+@Component
+public class ExchangePrice implements IExchange<PriceDTO> {
+
+    private final ISender<PriceDTO> sender;
+    private final IPriceService priceService;
+
+    @Autowired
+    public ExchangePrice(ISender<PriceDTO> sender, IPriceService priceService) {
+        this.sender = sender;
+        this.priceService = priceService;
+    }
+
+    @Override
+    public void exchange(PriceDTO priceDTO) {
+        priceService.deleteByExternalId(priceDTO.getExternalId());
+        HttpStatus status = sender.send(priceDTO);
+        if (status != HttpStatus.CREATED) {
+            // Save database.
+            priceService.saveDTO(priceDTO);
+        }
+    }
+}
