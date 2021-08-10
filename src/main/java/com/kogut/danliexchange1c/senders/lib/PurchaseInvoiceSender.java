e53ab188ba15ab83357514924b0daf26ea3e01f9
Kogut <koguts@yandex.ru>
2021-08-10 20:59:47 +0300

    add purchase invoice sender

--- src/main/java/com/kogut/danliexchange1c/senders/lib/PurchaseInvoiceSender.java
+++ src/main/java/com/kogut/danliexchange1c/senders/lib/PurchaseInvoiceSender.java
@@ -1,7 +1,51 @@
 package com.kogut.danliexchange1c.senders.lib;
 
+import com.kogut.danliexchange1c.dto.document.purchaseinvoice.PurchaseInvoiceDTO;
+import com.kogut.danliexchange1c.dto.lib.project.ProjectDTO;
+import com.kogut.danliexchange1c.enumerations.general.ClientDBEnum;
+import com.kogut.danliexchange1c.senders.interfaces.ISender;
+import com.kogut.danliexchange1c.util.interfaces.ISettingsUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+
 /**
  * @author S.Kogut on 10.08.2021
  */
-public class PurchaseInvoiceSender {
+
+@Component
+public class PurchaseInvoiceSender implements ISender<PurchaseInvoiceDTO> {
+
+    private final RestTemplate restTemplate;
+    private final ISettingsUtil settingsUtil;
+
+    @Autowired
+    public PurchaseInvoiceSender(RestTemplate restTemplate, ISettingsUtil settingsUtil) {
+        this.restTemplate = restTemplate;
+        this.settingsUtil = settingsUtil;
+    }
+
+    @Override
+    public HttpStatus send(PurchaseInvoiceDTO purchaseInvoiceDTO) {
+        HashMap<String, String> settings = settingsUtil.getSettings(ClientDBEnum.valueOf(purchaseInvoiceDTO.getClientDB()));
+        StringBuilder url = new StringBuilder();
+        url.append(settings.get("address")).append("/").append(settings.get("pathPurchaseInvoice"));
+        HttpEntity<PurchaseInvoiceDTO> request = new HttpEntity<>(purchaseInvoiceDTO, settingsUtil.createHeaders(settings.get("userName"), settings.get("password")));
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(
+                    url.toString(),
+                    request,
+                    String.class);
+            return response.getStatusCode();
+        } catch (IllegalArgumentException | RestClientException e) {
+            return HttpStatus.BAD_REQUEST;
+        }
+    }
+
 }
