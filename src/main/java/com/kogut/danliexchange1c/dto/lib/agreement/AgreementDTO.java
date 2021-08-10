328c42097d5e538a6e566d6801f15b8461d79da7
Kogut <koguts@yandex.ru>
2021-08-10 13:32:55 +0300

    add Agreement dto

---
+++ src/main/java/com/kogut/danliexchange1c/dto/lib/agreement/AgreementDTO.java
@@ -0,0 +1,25 @@
+package com.kogut.danliexchange1c.dto.lib.agreement;
+
+import com.kogut.danliexchange1c.dto.lib.common.AbstractCommonLibDTO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author S.Kogut on 10.08.2021
+ */
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class AgreementDTO extends AbstractCommonLibDTO {
+
+    @NotNull
+    @NotEmpty
+    private String type;
+    private String currencyId = "";
+
+}
---
+++ src/main/java/com/kogut/danliexchange1c/enumerations/lib/TypeAgreementEnum.java
@@ -0,0 +1,9 @@
+package com.kogut.danliexchange1c.enumerations.lib;
+
+/**
+ * @author S.Kogut on 10.08.2021
+ */
+public enum TypeAgreementEnum {
+    SELLER,
+    BUYER
+}
