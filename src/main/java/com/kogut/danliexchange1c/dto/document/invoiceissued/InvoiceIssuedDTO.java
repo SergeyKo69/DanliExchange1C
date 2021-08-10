ccd3ce06a4af06686e1326cad5541e67a68d6118
Kogut <koguts@yandex.ru>
2021-08-10 18:46:11 +0300

    add InvoiceIssued dto

---
+++ src/main/java/com/kogut/danliexchange1c/dto/document/invoiceissued/InvoiceIssuedDTO.java
@@ -0,0 +1,41 @@
+package com.kogut.danliexchange1c.dto.document.invoiceissued;
+
+import com.kogut.danliexchange1c.dto.document.common.AbstractCommonDocDTO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author S.Kogut on 10.08.2021
+ */
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class InvoiceIssuedDTO extends AbstractCommonDocDTO {
+
+    private Boolean correction;
+    private String kodeViewOperation = "";
+    private String kppClient = "";
+    private LocalDate dateOfIssue;
+    private String clientId;
+    private String agreementId = "";
+    private String viewInvoiceIssued = "";
+    private String docOwnerId = "";
+    private String docOwnerView = "";
+    private String viewOperationDocOwner = "";
+    private Integer rateVat = 20;
+    private BigDecimal amount = BigDecimal.ZERO;
+    private BigDecimal amountVat = BigDecimal.ZERO;
+    private LocalDate datePaymentSettlementDoc;
+    private String numberPaymentSettlementDoc = "";
+    private String numberCorrection = "";
+    private BigDecimal amountDoc = BigDecimal.ZERO;
+    private BigDecimal amountVatDoc = BigDecimal.ZERO;
+    private List<InvoiceIssuedPrepaymentTabDTO> prepayment = new ArrayList<>();
+}
