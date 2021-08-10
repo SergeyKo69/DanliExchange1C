31df9d93b5714f09c976dec1f902ab9332113d9c
Kogut <koguts@yandex.ru>
2021-08-10 14:03:22 +0300

    add agreement mapper

---
+++ src/main/java/com/kogut/danliexchange1c/mapper/lib/AgreementMapper.java
@@ -0,0 +1,50 @@
+package com.kogut.danliexchange1c.mapper.lib;
+
+import com.kogut.danliexchange1c.dto.lib.agreement.AgreementDTO;
+import com.kogut.danliexchange1c.dto.lib.project.ProjectDTO;
+import com.kogut.danliexchange1c.enumerations.general.ClientDBEnum;
+import com.kogut.danliexchange1c.enumerations.lib.TypeAgreementEnum;
+import com.kogut.danliexchange1c.mapper.abstracted.AbstractMapper;
+import com.kogut.danliexchange1c.model.lib.agreement.AgreementEntity;
+import com.kogut.danliexchange1c.model.lib.project.ProjectEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author S.Kogut on 10.08.2021
+ */
+
+@Component
+public class AgreementMapper extends AbstractMapper<AgreementEntity, AgreementDTO> {
+
+    private final ModelMapper mapper;
+
+    @Autowired
+    public AgreementMapper(ModelMapper mapper) {
+        super(AgreementEntity.class, AgreementDTO.class);
+        this.mapper = mapper;
+    }
+
+    @PostConstruct
+    public void setupMapper() {
+        mapper.createTypeMap(AgreementDTO.class, AgreementEntity.class)
+                .addMappings(m -> m.skip(AgreementEntity:: setId))
+                .addMappings(m -> m.skip(AgreementEntity::setClientDB));
+    }
+
+    @Override
+    public void mapSpecificFields(AgreementEntity source, AgreementDTO destination) {
+        super.mapSpecificFields(source, destination);
+    }
+
+    @Override
+    public void mapSpecificFields(AgreementDTO source, AgreementEntity destination) {
+        super.mapSpecificFields(source, destination);
+        destination.setClientDB(ClientDBEnum.valueOf(source.getClientDB()));
+        destination.setType(TypeAgreementEnum.valueOf(source.getType()));
+    }
+
+}
