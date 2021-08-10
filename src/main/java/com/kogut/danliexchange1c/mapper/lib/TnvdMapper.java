2f0cd202d8bbd4aa2830bcc939d17f57936d6921
Kogut <koguts@yandex.ru>
2021-08-11 00:26:32 +0300

    add tnvd mapper

---
+++ src/main/java/com/kogut/danliexchange1c/mapper/lib/TnvdMapper.java
@@ -0,0 +1,48 @@
+package com.kogut.danliexchange1c.mapper.lib;
+
+import com.kogut.danliexchange1c.dto.lib.store.StoreDTO;
+import com.kogut.danliexchange1c.dto.lib.tnvd.TnvdDTO;
+import com.kogut.danliexchange1c.enumerations.general.ClientDBEnum;
+import com.kogut.danliexchange1c.mapper.abstracted.AbstractMapper;
+import com.kogut.danliexchange1c.model.lib.store.StoreEntity;
+import com.kogut.danliexchange1c.model.lib.tnvd.TnvdEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author S.Kogut on 11.08.2021
+ */
+
+@Component
+public class TnvdMapper extends AbstractMapper<TnvdEntity, TnvdDTO> {
+
+    private final ModelMapper mapper;
+
+    @Autowired
+    public TnvdMapper(ModelMapper mapper) {
+        super(TnvdEntity.class, TnvdDTO.class);
+        this.mapper = mapper;
+    }
+
+    @PostConstruct
+    public void setupMapper() {
+        mapper.createTypeMap(TnvdDTO.class, TnvdEntity.class)
+                .addMappings(m -> m.skip(TnvdEntity:: setId))
+                .addMappings(m -> m.skip(TnvdEntity::setClientDB));
+    }
+
+    @Override
+    public void mapSpecificFields(TnvdEntity source, TnvdDTO destination) {
+        super.mapSpecificFields(source, destination);
+    }
+
+    @Override
+    public void mapSpecificFields(TnvdDTO source, TnvdEntity destination) {
+        super.mapSpecificFields(source, destination);
+        destination.setClientDB(ClientDBEnum.valueOf(source.getClientDB()));
+    }
+
+}
