730bbf68d3e61519b86c60e8b6af74505c0591e7
Kogut <koguts@yandex.ru>
2021-08-11 00:22:35 +0300

    add tnvd dto

---
+++ src/main/java/com/kogut/danliexchange1c/dto/lib/tnvd/TnvdDTO.java
@@ -0,0 +1,16 @@
+package com.kogut.danliexchange1c.dto.lib.tnvd;
+
+import com.kogut.danliexchange1c.dto.lib.common.AbstractCommonLibDTO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author S.Kogut on 11.08.2021
+ */
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class TnvdDTO extends AbstractCommonLibDTO {
+}
