4a42c27ed2a80434865ef3c1a4f8e0fd49c82bc2
Kogut <koguts@yandex.ru>
2021-08-11 15:01:35 +0300

    add invoice received service

---
+++ src/main/java/com/kogut/danliexchange1c/services/db/document/implement/InvoiceReceivedService.java
@@ -0,0 +1,86 @@
+package com.kogut.danliexchange1c.services.db.document.implement;
+
+import com.kogut.danliexchange1c.dto.document.invoicereceived.InvoiceReceivedDTO;
+import com.kogut.danliexchange1c.enumerations.general.ClientDBEnum;
+import com.kogut.danliexchange1c.mapper.document.InvoiceReceivedMapper;
+import com.kogut.danliexchange1c.model.document.invoicereceived.InvoiceReceivedEntity;
+import com.kogut.danliexchange1c.repositories.document.InvoiceReceivedRepository;
+import com.kogut.danliexchange1c.services.db.document.interfaces.IInvoiceReceivedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author S.Kogut on 11.08.2021
+ */
+
+@Service
+public class InvoiceReceivedService implements IInvoiceReceivedService {
+
+    private final InvoiceReceivedRepository invoiceReceivedRepository;
+    private final InvoiceReceivedMapper invoiceReceivedMapper;
+
+    @Autowired
+    public InvoiceReceivedService(InvoiceReceivedRepository invoiceReceivedRepository, InvoiceReceivedMapper invoiceReceivedMapper) {
+        this.invoiceReceivedRepository = invoiceReceivedRepository;
+        this.invoiceReceivedMapper = invoiceReceivedMapper;
+    }
+
+    @Override
+    @Transactional
+    public InvoiceReceivedEntity save(InvoiceReceivedEntity entity) {
+        return invoiceReceivedRepository.save(entity);
+    }
+
+    @Override
+    @Transactional
+    public InvoiceReceivedEntity saveDTO(InvoiceReceivedDTO dto) {
+        return invoiceReceivedRepository.save(invoiceReceivedMapper.toEntity(dto));
+    }
+
+    @Override
+    @Transactional
+    public List<InvoiceReceivedEntity> saveAll(List<InvoiceReceivedEntity> entity) {
+        return invoiceReceivedRepository.saveAll(entity);
+    }
+
+    @Override
+    public List<InvoiceReceivedEntity> findAll() {
+        return invoiceReceivedRepository.findAll();
+    }
+
+    @Override
+    public List<InvoiceReceivedEntity> findAllByExternalId(String externalId) {
+        return invoiceReceivedRepository.findAllByExternalId(externalId);
+    }
+
+    @Override
+    public List<InvoiceReceivedEntity> findAllByClientDBOrderByDateTime(ClientDBEnum clientDB) {
+        return invoiceReceivedRepository.findAllByClientDBOrderByDateTime(clientDB);
+    }
+
+    @Override
+    public List<InvoiceReceivedDTO> findAllByClientDBOrderByDateTimeDTO(ClientDBEnum clientDB) {
+        return invoiceReceivedRepository
+                .findAllByClientDBOrderByDateTime(clientDB)
+                .parallelStream()
+                .map(invoiceReceivedMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public void deleteByExternalId(String externalId) {
+        invoiceReceivedRepository.deleteByExternalId(externalId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteAll() {
+        invoiceReceivedRepository.deleteAll();
+    }
+
+}
---
+++ src/main/java/com/kogut/danliexchange1c/services/db/document/interfaces/IInvoiceReceivedService.java
@@ -0,0 +1,10 @@
+package com.kogut.danliexchange1c.services.db.document.interfaces;
+
+import com.kogut.danliexchange1c.dto.document.invoicereceived.InvoiceReceivedDTO;
+import com.kogut.danliexchange1c.model.document.invoicereceived.InvoiceReceivedEntity;
+
+/**
+ * @author S.Kogut on 11.08.2021
+ */
+public interface IInvoiceReceivedService extends IBaseDocService<InvoiceReceivedEntity, InvoiceReceivedDTO> {
+}
