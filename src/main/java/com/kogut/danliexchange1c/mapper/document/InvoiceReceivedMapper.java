1f1bc8b00c9c7241e08d5668ffd7ebc86ba481ec
Kogut <koguts@yandex.ru>
2021-08-11 14:22:50 +0300

    add invoice received mapper

---
+++ src/main/java/com/kogut/danliexchange1c/mapper/document/InvoiceReceivedMapper.java
@@ -0,0 +1,46 @@
+package com.kogut.danliexchange1c.mapper.document;
+
+import com.kogut.danliexchange1c.dto.document.invoicereceived.InvoiceReceivedDTO;
+import com.kogut.danliexchange1c.enumerations.general.ClientDBEnum;
+import com.kogut.danliexchange1c.mapper.abstracted.AbstractMapper;
+import com.kogut.danliexchange1c.model.document.invoicereceived.InvoiceReceivedEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author S.Kogut on 11.08.2021
+ */
+
+@Component
+public class InvoiceReceivedMapper extends AbstractMapper<InvoiceReceivedEntity, InvoiceReceivedDTO> {
+
+    private final ModelMapper mapper;
+
+    @Autowired
+    public InvoiceReceivedMapper(ModelMapper mapper) {
+        super(InvoiceReceivedEntity.class, InvoiceReceivedDTO.class);
+        this.mapper = mapper;
+    }
+
+    @PostConstruct
+    public void setupMapper() {
+        mapper.createTypeMap(InvoiceReceivedDTO.class, InvoiceReceivedEntity.class)
+                .addMappings(m -> m.skip(InvoiceReceivedEntity :: setId))
+                .addMappings(m -> m.skip(InvoiceReceivedEntity :: setClientDB));
+    }
+
+    @Override
+    public void mapSpecificFields(InvoiceReceivedEntity source, InvoiceReceivedDTO destination) {
+        super.mapSpecificFields(source, destination);
+    }
+
+    @Override
+    public void mapSpecificFields(InvoiceReceivedDTO source, InvoiceReceivedEntity destination) {
+        super.mapSpecificFields(source, destination);
+        destination.setClientDB(ClientDBEnum.valueOf(source.getClientDB()));
+    }
+
+}
