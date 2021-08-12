5760f45927f20c6b2a0bfec136f131f11ec183ce
Kogut <koguts@yandex.ru>
2021-08-12 16:38:03 +0300

    add bank statement service

---
+++ src/main/java/com/kogut/danliexchange1c/services/db/document/implement/BankStatementService.java
@@ -0,0 +1,85 @@
+package com.kogut.danliexchange1c.services.db.document.implement;
+
+import com.kogut.danliexchange1c.dto.document.bankstatement.BankStatementDTO;
+import com.kogut.danliexchange1c.enumerations.general.ClientDBEnum;
+import com.kogut.danliexchange1c.mapper.document.BankStatementMapper;
+import com.kogut.danliexchange1c.model.document.bankstatement.BankStatementEntity;
+import com.kogut.danliexchange1c.repositories.document.BankStatementRepository;
+import com.kogut.danliexchange1c.services.db.document.interfaces.IBankStatementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author S.Kogut on 12.08.2021
+ */
+
+@Service
+public class BankStatementService implements IBankStatementService {
+
+    private final BankStatementRepository bankStatementRepository;
+    private final BankStatementMapper bankStatementMapper;
+
+    @Autowired
+    public BankStatementService(BankStatementRepository bankStatementRepository,
+                                BankStatementMapper bankStatementMapper) {
+        this.bankStatementRepository = bankStatementRepository;
+        this.bankStatementMapper = bankStatementMapper;
+    }
+
+    @Override
+    @Transactional
+    public BankStatementEntity save(BankStatementEntity entity) {
+        return bankStatementRepository.save(entity);
+    }
+
+    @Override
+    @Transactional
+    public BankStatementEntity saveDTO(BankStatementDTO dto) {
+        return bankStatementRepository.save(bankStatementMapper.toEntity(dto));
+    }
+
+    @Override
+    @Transactional
+    public List<BankStatementEntity> saveAll(List<BankStatementEntity> entity) {
+        return bankStatementRepository.saveAll(entity);
+    }
+
+    @Override
+    public List<BankStatementEntity> findAll() {
+        return bankStatementRepository.findAll();
+    }
+
+    @Override
+    public List<BankStatementEntity> findAllByExternalId(String externalId) {
+        return bankStatementRepository.findAllByExternalId(externalId);
+    }
+
+    @Override
+    public List<BankStatementEntity> findAllByClientDBOrderByDateTime(ClientDBEnum clientDB) {
+        return bankStatementRepository.findAllByClientDBOrderByDateTime(clientDB);
+    }
+
+    @Override
+    public List<BankStatementDTO> findAllByClientDBOrderByDateTimeDTO(ClientDBEnum clientDB) {
+        return bankStatementRepository.findAllByClientDBOrderByDateTime(clientDB)
+                .parallelStream()
+                .map(bankStatementMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public void deleteByExternalId(String externalId) {
+        bankStatementRepository.deleteByExternalId(externalId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteAll() {
+        bankStatementRepository.deleteAll();
+    }
+}
---
+++ src/main/java/com/kogut/danliexchange1c/services/db/document/interfaces/IBankStatementService.java
@@ -0,0 +1,10 @@
+package com.kogut.danliexchange1c.services.db.document.interfaces;
+
+import com.kogut.danliexchange1c.dto.document.bankstatement.BankStatementDTO;
+import com.kogut.danliexchange1c.model.document.bankstatement.BankStatementEntity;
+
+/**
+ * @author S.Kogut on 12.08.2021
+ */
+public interface IBankStatementService extends IBaseDocService<BankStatementEntity, BankStatementDTO> {
+}
